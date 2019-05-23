1ebad5c5e630a724238f4934b81a80dc2edb41e5
Instafever <495140158@qq.com>
2019-05-23 16:57:41 +0800

    用户个人信息修改功能实现

--- app/zsg/user_info.py
+++ app/zsg/user_info.py
@@ -1,26 +1,24 @@
 """
 本模块包括：
-个人信息展示
 个人信息修改
 """
 import datetime
 import os
 
-from flask import redirect, render_template, request, jsonify
+from flask import redirect, request, jsonify
 
 from app import db
-from app.common.funs import chkLogin, ret_sucess, ret_error
+from app.common.funs import chkLogin
 from app.models import User
 from app.zsg.form_check import FormCheck
 from . import zsg
 
 
-@zsg.route("/user_info", methods=['GET', 'POST'])
+@zsg.route("/user_info", methods=['POST'])
 def user_info():
     """
-    用户个人信息视图函数
-    :return: get 用户个人信息界面
-             post 修改成功
+    用户个人信息修改视图函数
+    :return: 成功code:1/失败code:0
     """
 
     # 检查用户是否处于登录状态，如果不是，跳转到登录界面
@@ -29,23 +27,24 @@ def user_info():
         return redirect('/login')
 
     user = User.query.filter_by(user_no=user_no).first()
-    if request.method == 'GET':
-        # 返回用户个人信息
-        return render_template("index.html", user=user)
-    else:
-        # 修改用户个人信息
-        user.user_nick_name = request.form['user_nick_name']
-        user.user_tel = request.form['user_tel']
-        user.user_email = request.form['user_email']
-        # 检查用户信息
-        res = info_check(user.user_nick_name, user.user_tel, user.user_email)
-        if res != True:
-            return ret_error(res)
 
-        # 保存用户信息
-        db.session.add(user)
+    # 获取表单数据
+    user_nick_name = request.form['user_nick_name']
+    user_tel = request.form['user_tel']
+    user_email = request.form['user_email']
+    # 表单数据校验
+    res = info_check(user_nick_name, user_tel, user_email, user)
+    if res != True:
+        return jsonify({'code': 0, 'msg': res})
 
-        return ret_sucess("修改成功")
+    # 修改用户个人信息
+    user.user_nick_name = user_nick_name
+    user.user_tel = user_tel
+    user.user_email = user_email
+    # 保存用户信息
+    db.session.add(user)
+
+    return jsonify({'code': 1, 'msg': '保存成功'})
 
 
 @zsg.route('/user_head_upload', methods=['POST'])
@@ -77,27 +76,31 @@ def user_head_upload():
         user.user_head_pic = pic_path
         user.pic_name = pic_name
         db.session.add(user)
+        return jsonify({'code': 1, 'msg': '上传成功'})
 
-    return jsonify({'code': 1, 'msg': '上传成功'})
+    return jsonify({'code': 0, 'msg': '上传失败'})
 
 
-def info_check(name, tel, email):
+def info_check(name, tel, email, user):
     """
-    检查用户信息
+    检查表单数据
     :param name: 用户名
     :param tel: 手机号
     :param email: 邮箱
+    :param user: 用户实体
     :return: 错误信息/True
     """
 
+    res = True
     # 检查用户名
-    res = FormCheck.name_check(name)
-    if res == True:
-        # 检查电话号
+    if user.user_nick_name != name:
+        res = FormCheck.name_check(name)
+    # 检查电话号
+    if res == True and user.user_tel != tel and tel != '':
         res = FormCheck.tel_check(tel)
-        if res == True:
-            # 检查邮箱
-            res = FormCheck.email_check(email)
+    # 检查邮箱
+    if res == True and user.user_email != email and email != '':
+        res = FormCheck.email_check(email)
     return res
 
 
